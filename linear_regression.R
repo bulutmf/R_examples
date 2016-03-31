cf366f35302855a785eb455c762d0602579d2e42
Muhammed Fatih Bulut <mfbulut@us.ibm.com>
2016-03-31 12:42:49 -0400

    First commit

---
+++ linear_regression.R
@@ -0,0 +1,23 @@
+
+height <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+weight <- c(63, 81, 56, 91, 47, 57, 76, 72, 62, 48)
+data <- data.frame(height, weight)
+
+
+
+#TODO
+# Plot height vs. weight
+
+
+# TODO
+# Create linear regression model using "lm" function
+
+
+
+# Given the heights predict weights
+newHeights = c(140, 150, 180, 190)
+newData = data.frame(height=newHeights)
+# TODO predict weights
+
+
+
